607f7acd4492c2b2b1798afa7582b2155cd4def9
Alexander Kravchenko <alex.kravchenko@protonmail.com>
2022-01-14 15:58:54 +0200

    progress update

--- app/src/main/java/com/okravi/loconotes/activities/MainActivity.kt
+++ app/src/main/java/com/okravi/loconotes/activities/MainActivity.kt
@@ -3,10 +3,19 @@ package com.okravi.loconotes.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.okravi.loconotes.R
+import com.okravi.loconotes.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private var binding : ActivityMainBinding? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        setContentView(binding?.root)
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding = null
     }
 }
